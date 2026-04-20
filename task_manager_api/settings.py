4a013c277e4bc3b0551da717ca96e2f3eb3515d9
Ojochenemi <johnamos896@gmail.com>
2026-04-20 15:37:15 +0100

    fix static root for deployment

--- task_manager_api/settings.py
+++ task_manager_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
